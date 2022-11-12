87f203f71dd19280d71ecff089198e6a4958a537
Ray Young <ray.arden.young@gmail.com>
2022-11-12 09:51:55 -0500

    book detail showing title instead of pk

--- library/serializers.py
+++ library/serializers.py
@@ -4,6 +4,7 @@ from .models import Book, Note
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        #these fields are the bits of data that will populate in insomnia
         fields = ['title', 'author', 'published_date', 'genre', 'featured' ]
 
 class FeaturedSerializer(serializers.ModelSerializer):
@@ -12,15 +13,18 @@ class FeaturedSerializer(serializers.ModelSerializer):
         fields = ['title', 'author', 'published_date', 'genre']
 
 class NoteSerializer(serializers.ModelSerializer):
+    #direct it to source so that 'book' will show up as a string instead of pk
+    book = serializers.CharField(source='book.title')
     class Meta:
         model = Note
-        fields = ['entry_name', 'entry', 'date', 'book']
+        fields = ['entry_name', 'book', 'date', 'entry']
 
-#accessing foreign key
+
 class BookDetailSerializer(serializers.ModelSerializer):
-    notes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    #accessing foreign keys
     statuses = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    notes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Book
-        fields = ['title', 'author', 'published_date', 'genre', 'featured', 'notes', 'statuses']
+        fields = ['title', 'author', 'published_date', 'genre', 'featured', 'statuses', 'notes']
